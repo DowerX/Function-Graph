549f430722e6683deb8fe02347be1448cbbe923f
Benedek László <41867355+DowerX@users.noreply.github.com>
2019-02-12 20:43:42 +0100

    Added math and try

--- calc_func.py
+++ calc_func.py
@@ -3,20 +3,26 @@
 import draw_func
 from PIL import Image
 
-size = 10
+size = 50
 
 def calculate(func):
     _maxsize = (100 * 2)+ 2
     #write into file to execute string
     with open("./func.py", "w") as f:
-        f.write("def f(x):\n    return {}".format(func))
+        f.write("import math\n\ndef f(x):\n    return {}".format(func))
         f.close()
 
     #import and run
-    import func
+    try:
+        import func
+    except:
+        print("Error importing custom function! Missing file or syntax error?")
     result = []
     for x in range(-size, size+1):
-        y = func.f(x)
+        try:
+            y = func.f(x)
+        except :
+            print("Syntax error! Remember to use correct Python 3 math syntax!")
         result.append((y, x))
         if(y > _maxsize):
             _maxsize = (y*2) + 2
